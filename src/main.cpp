33ba6a1199c10a664e5c77a4ac9e0d02e2a3c081
sfrezza <sfrezza@franciscan.edu>
2023-02-14 11:35:19 -0500

    Reorg with Makefile. Linker errors

---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "Board.hpp"
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, CCBB World!\n";
+    Board *myBoard = new Board();
+    
+    return 0;
+}
