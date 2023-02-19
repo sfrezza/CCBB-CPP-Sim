cacee98e84e88794de2c2337d357802e4ea52516
Steve Frezza <frezza001@gannon.edu>
2023-02-18 23:08:53 -0500

    Corrections to main.cpp for gtest use

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,20 @@
 #include <iostream>
+#include <gtest/gtest.h>
+
 #include "Board.hpp"
 
-int main(int argc, const char * argv[]) {
+TEST(FakeTest, SFrezzaFakeTesting) {
+    EXPECT_STREQ("Red", "Red");
+}
+
+
+
+int main(int argc, char * argv[]) {
     // insert code here...
     std::cout << "Hello, CCBB World!\n";
     Board *myBoard = new Board();
+
+    testing::InitGoogleTest(&argc, argv);
     
-    return 0;
+    return RUN_ALL_TESTS();
 }
