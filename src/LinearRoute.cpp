e82fc147a1868ffb2829e0071dff0df80bba8d7f
sfrezza <sfrezza@franciscan.edu>
2023-03-25 17:17:45 -0400

    Bug in LinearRoute Constructor.

--- src/LinearRoute.cpp
+++ src/LinearRoute.cpp
@@ -12,9 +12,9 @@
 LinearRoute::LinearRoute(int length) : Route() {
     endSpace = new Space("End");
     for (int index=0; index<length; index++) {
-        Space newSpace = Space("Purple");
+        Space *newSpace = new Space("Purple");
         newSpace.myRoute = this;
-        path.push_back(&newSpace);
+        path.push_back(newSpace);
     }
     endSpace->myRoute = this;
 }
