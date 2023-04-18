f8812ca427e65eadc67b4df2e2ad2e8a98aaea7e
sfrezza <sfrezza@franciscan.edu>
2023-04-18 19:31:51 -0400

    Baseline rapidcheck tests

--- tests/RoutePropertyTest.cpp
+++ tests/RoutePropertyTest.cpp
@@ -17,41 +17,55 @@
 namespace my {
 namespace project {
 
+RC_GTEST_PROP(MyTestCase,
+              copyOfStringIsIdenticalToOriginal,
+              (const std::string &str)) {
+  const auto strCopy = str;
+  RC_ASSERT(strCopy == str);
+}
+
+/*
 // Tests that the Player::Player() constructor method does what it should.
 RC_GTEST_PROP(MyRapidPlayerTest, ConstructorWorksAllColors, ()) {
     LinearRoute *someRoute = new LinearRoute(5);
     Space *someSpace = someRoute->startSpace;
-    //const auto testColor = *rc::gen::build(
+//    const auto testColor = *rc::gen::build(
     //    // List all of the acceptable colors
-    //    rc::gen::element("Red","Blue","Green","Yellow"));
+        rc::gen::element("Red","Blue","Green","Yellow"));
 
     Player *p = new Player("Red", someSpace);
     RC_ASSERT(p->currentSpace == someSpace);
-    //RC_ASSERT(p->myColor.compare(testColor) == 0);
-}
-
-/*
-// Should succeed:
-RC_GTEST_PROP(MyTestCase, firstMoves,()) {
-// Properties to vary:
-   const auto playerNumber = *rc::gen::inRange(0,3);
-   const auto dieRoll = *rc::gen::inRange(1,6);
-
-// Test setup:
-   Board *b = new Board();
-   Player *p;
-   RC_ASSERT(p=b->players[playerNumber]->currentSpace == b->route1->startSpace);
-
-   Space *endSpace = b->route1->movePlayer(p,dieRoll); // This should reset the current space, etc.
-   std::set<Player*> playersOnEndSpace = endSpace->currentPlayers;
-   RC_ASSERT_TRUE(playersOnEndSpace.find(p) != playersOnEndSpace.end() );
-
-   std::set<Player*> playersOnStartSpace = b->route1->startSpace->currentPlayers;
-   RC_ASSERT_TRUE(playersOnStartSpace.find(p) == playersOnStartSpace.end() );
-
-   RC_ASSERT_EQ(b->route1->path[dieRoll-1],p->currentSpace);
+    RC_ASSERT(p->myColor.compare(testColor) == 0);
 }
 */
+
+
+RC_GTEST_PROP(RandomizeMoveTestCase, firstMoves,()) {
+// Test setup:
+   Board *b = new Board();
+
+// Properties to vary:
+   const auto playerNumber = *rc::gen::inRange(0,5);
+   const auto dieRoll = *rc::gen::inRange(2,4);
+
+   Player *p = b->players[playerNumber];;
+   RC_ASSERT(p->currentSpace == b->route1->startSpace);
+
+   std::set<Player*> playersOnStartSpace = b->route1->startSpace->currentPlayers;
+   RC_ASSERT(playersOnStartSpace.find(p) != playersOnStartSpace.end() );
+   RC_ASSERT(playersOnStartSpace.count(p) == 1);
+
+   Space *endSpace = b->route1->movePlayer(p,dieRoll); // This should reset the current space, etc.
+
+   RC_ASSERT(b->route1->path[dieRoll-1]== p->currentSpace);
+   std::set<Player*> playersOnEndSpace = endSpace->currentPlayers;
+   RC_ASSERT(playersOnEndSpace.find(p) != playersOnEndSpace.end() );
+   RC_ASSERT(playersOnEndSpace.count(p) == 1);
+
+   std::set<Player*> remainingPlayersOnStartSpace = b->route1->startSpace->currentPlayers;
+   RC_ASSERT(remainingPlayersOnStartSpace.count(p) == 0);
+}
+
 
 
 }  // namespace project
